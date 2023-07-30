8811a5cd4f6cd4bef84ce92bc1d6d198ce236f8c
BoxenOfDonuts <joel.hacke@gmail.com>
2023-07-30 15:27:18 -0500

    get env from file or env

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,22 @@
 import { load } from "#deps";
 const env = await load();
 
-const apiKey = env["DATADOG_API_KEY"];
-const appKey = env["DATADOG_APP_KEY"];
-const weatherApiKey = env["WEATHER_API_KEY"];
+const apiKey = getEnv("DATADOG_API_KEY");
+const appKey = getEnv("DATADOG_APP_KEY");
+const weatherApiKey = getEnv("WEATHER_API_KEY");
 const defaultSleep = 2 * 60 * 1000; // 2 minutes
 const itsDarkSleep = 60 * 60 * 1000; // 1 hour
 const thirtyMinutes = 30 * 60 * 1000; // 30 minutes
+
+function getEnv(key: string): string {
+  const result = env[key] || Deno.env.get(key);
+  if (!result) {
+    throw new Error(
+      `Missing env var ${key}, make sure you have a .env file or you pass it in via the command line`,
+    );
+  }
+  return result;
+};
 
 export {
   apiKey,
